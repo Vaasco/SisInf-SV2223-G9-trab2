396e88010e9430c944dd1a9aaed3bb7ab1be06a2
Vaasco <vascobranco13@gmail.com>
2023-05-19 15:46:42 +0100

    -Second commit

--- src/main/java/model/ComprarId.java
+++ src/main/java/model/ComprarId.java
@@ -2,7 +2,6 @@ package model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Entity;
 
 import java.io.Serializable;
 import java.util.Objects;
